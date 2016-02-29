e99b5aeec3ee9f217f8730fd31b985491150d2de
yzhang <yi.zhang@thpii.com>
2016-02-29 13:33:37 -0800

    add new method

--- src/Solution.java
+++ src/Solution.java
@@ -4296,6 +4296,31 @@ public class Solution {
     }
 
 
+    /**
+     *Given an array of integers and an integer k, find out whether
+     * there are two distinct indices i and j in the array such that
+     * nums[i] = nums[j] and the difference between i and j is
+     *
+     * 
+     */
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i=0; i<nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i-k-1]);
+            }
+
+            if (!set.contains(nums[i])) {
+                set.add(nums[i]);
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
 
 
 
