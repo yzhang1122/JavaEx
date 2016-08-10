7bbd6fb77ef78d2b5a550b2ce0805a20aacfe709
yzhang <yi.zhang@thpii.com>
2016-08-09 20:40:11 -0700

    add 3sum closest

--- src/Solution.java
+++ src/Solution.java
@@ -302,6 +302,38 @@ public class Solution {
     }
 
     /*
+     *  3Sum Closest
+     *  Given an array S of n integers, find three integers in S such that
+     *  the sum is closest to a given number, target.
+     *  Return the sum of the three integers. You may assume that
+     *  each input would have exactly one solution.
+     *
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3) {
+            return -1;
+        }
+        Arrays.sort(nums);
+        int bestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1, end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - sum) < Math.abs(target - bestSum)) {
+                    bestSum = sum;
+                }
+                if (sum < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+
+        return bestSum;
+    }
+
+    /*
      *  Next Permutation
      *  1,2,3 → 1,3,2
      *  3,2,1 → 1,2,3
